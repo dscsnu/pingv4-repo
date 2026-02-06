73f1a22bbffc48b30517913f56dfb5fa88fb186e
Hsekumsti@gmail.com <Hsekumsti@gmail.com>
2026-02-06 23:04:50 +0530

    Update MP282 bot with Negamax Alpha-Beta algorithm

--- main.py
+++ main.py
@@ -1,48 +1,16 @@
-"""
-A test runner.
-
-HOW TO USE:
-1. Create your bot in submissions/yourname_yournetid.py
-2. Change the import line to import YOUR bot
-3. Run python main.py
-"""
-
-from pingv4 import Connect4Game, MinimaxBot, RandomBot
-from submissions import template_bot.py # Change this line to import yourname_yournetid.py
+from pingv4 import Connect4Game, RandomBot, MinimaxBot
+from submissions.mukesh_mp282 import MP282
 
 def main():
-  bot = Bot()
-  print("=" * 50)
-  print(f"Testing Bot: {bot.strategy_name}")
-  print(f"Author: {bot.author_name} {bot.author_netid}")
-  print("=" * 50)
-  print()
+    print("Connect Four: RandomBot vs MP282")
+    input("Press Enter to start")
 
-  # Test 1: Human vs Your Bot
-  print("Test: Human vs Your Bot")
-  input("Press Enter to start")
-  game = Connect4Game(player1=None, player2=Bot)
-  game.run()
+    game = Connect4Game(
+        player1=RandomBot,
+        player2=MP282
+    )
 
-  # Uncomment additional tests
-
-  # Test 3: Your Bot vs Random Bot
-  # print("Test: Your Bot vs Random Bot")
-  # input("Press Enter to start")
-  # game = Connect4Game(player1=Bot, player2=RandomBot")
-  # game.run()
-
-  # Test 34: Your Bot vs Minimax Bot
-  # print("Test: Your Bot vs Minimax Bot")
-  # input("Press Enter to start")
-  # game = Connect4Game(player1=Bot, player2=MinimaxBot")
-  # game.run()
-
-  # Test 3: Your Bot vs Your Bot
-  # print("Test: Your Bot vs Your Bot")
-  # input("Press Enter to start")
-  # game = Connect4Game(player1=Bot, player2=Bot")
-  # game.run()
+    game.run()
 
 if __name__ == "__main__":
-  main()
+    main()
