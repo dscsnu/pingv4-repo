6674be0481f5eda3b2be057132d33a2700738cd3
Saikarthik-Ramakrishnan <rsaik2606@gmail.com>
2026-02-06 23:18:58 +0530

    Add saikarthik_sr255 bot and update main.py for compatibility

--- main.py
+++ main.py
@@ -7,42 +7,44 @@ HOW TO USE:
 3. Run python main.py
 """
 
-from pingv4 import Connect4Game, MinimaxBot, RandomBot
-from submissions import template_bot.py # Change this line to import yourname_yournetid.py
+
+from pingv4 import Connect4Game
+from submissions.saikarthik_sr255 import saikarthik_sr255
+
 
 def main():
-  bot = Bot()
-  print("=" * 50)
-  print(f"Testing Bot: {bot.strategy_name}")
-  print(f"Author: {bot.author_name} {bot.author_netid}")
-  print("=" * 50)
-  print()
+    bot = saikarthik_sr255()
+    print("=" * 50)
+    print(f"Testing Bot: {bot.strategy_name}")
+    print(f"Author: {bot.author_name} {bot.author_netid}")
+    print("=" * 50)
+    print()
 
-  # Test 1: Human vs Your Bot
-  print("Test: Human vs Your Bot")
-  input("Press Enter to start")
-  game = Connect4Game(player1=None, player2=Bot)
-  game.run()
+    # Test 1: Human vs Your Bot
+    print("Test: Human vs Your Bot")
+    input("Press Enter to start")
+    game = Connect4Game(player1=None, player2=saikarthik_sr255)
+    game.run()
 
-  # Uncomment additional tests
+    # Uncomment additional tests
+    # Test 3: Your Bot vs Random Bot
+    # print("Test: Your Bot vs Random Bot")
+    # input("Press Enter to start")
+    # game = Connect4Game(player1=saikarthik_sr255, player2=RandomBot)
+    # game.run()
 
-  # Test 3: Your Bot vs Random Bot
-  # print("Test: Your Bot vs Random Bot")
-  # input("Press Enter to start")
-  # game = Connect4Game(player1=Bot, player2=RandomBot")
-  # game.run()
+    # Test 34: Your Bot vs Minimax Bot
+    # print("Test: Your Bot vs Minimax Bot")
+    # input("Press Enter to start")
+    # game = Connect4Game(player1=saikarthik_sr255, player2=MinimaxBot)
+    # game.run()
 
-  # Test 34: Your Bot vs Minimax Bot
-  # print("Test: Your Bot vs Minimax Bot")
-  # input("Press Enter to start")
-  # game = Connect4Game(player1=Bot, player2=MinimaxBot")
-  # game.run()
+    # Test 3: Your Bot vs Your Bot
+    # print("Test: Your Bot vs Your Bot")
+    # input("Press Enter to start")
+    # game = Connect4Game(player1=saikarthik_sr255, player2=saikarthik_sr255)
+    # game.run()
 
-  # Test 3: Your Bot vs Your Bot
-  # print("Test: Your Bot vs Your Bot")
-  # input("Press Enter to start")
-  # game = Connect4Game(player1=Bot, player2=Bot")
-  # game.run()
 
 if __name__ == "__main__":
-  main()
+    main()
